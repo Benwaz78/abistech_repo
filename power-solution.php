c5bd6cb97ae93cd82119fa2ca3f97b41b43f6beb
dutchkay1 <ductchkay7@gmail.com>
2021-02-23 15:58:05 +0100

    newly updated

---
+++ power-solution.php
@@ -0,0 +1,45 @@
+<?php require_once('contact_process.php') ?>
+<?php require_once('templates/header.php') ?>
+
+    <main>
+      <div class="slider-area ">
+          <!-- Mobile Menu -->
+          <div class="single-slider slider-height2 d-flex align-items-center" data-background="assets/img/hero/services_hero.jpg" style="background-image: url(&quot;assets/img/hero/services_hero.jpg&quot;);">
+              <div class="container">
+                  <div class="row">
+                      <div class="col-xl-12">
+                          <div class="hero-cap text-center">
+                              <h2>Power Solution</h2>
+                            <p class="text-white">We Plan, We Build, We Manage</p>   
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+        </div>
+        <section class="blog_area single-post-area service-content section-padding">
+              <div class="container">
+                 <div class="row no-gutters">
+                    <div class="col-md-6">
+                        <img src="assets/images/powersolution.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-6 p-4">
+                        <h2>Power Solution</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero nesciunt laudantium fugiat dolorum. Ipsam odio non libero i
+                            itaque quasi perspiciatis nobis, impedit, natus porro quos provident sapiente neque aspernatur.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero nesciunt laudantium fugiat dolorum. Ipsam odio non libero i
+                            itaque quasi perspiciatis nobis, impedit, natus porro quos provident sapiente neque aspernatur.
+                        </p>
+                    </div>
+                 </div>
+                 
+            </div>
+        </section>
+
+
+        <?php require_once('templates/contact-section.php') ?>
+    </main>
+<?php require_once('templates/footer.php') ?>
